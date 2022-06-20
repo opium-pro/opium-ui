1d2080a77740e095357f674580fca9328e1bab0f
Denis Elianovsky <denis@opium.pro>
2022-06-20 23:37:56 +0200

    time to datepicker

--- src/components/date-picker/index.tsx
+++ src/components/date-picker/index.tsx
@@ -3,16 +3,24 @@ import { TextInput, TextInputProps } from '../text-input'
 import dayjs from 'dayjs'
 
 
-export const DatePicker = (props: TextInputProps) => {
+export type DatePickerProps = TextInputProps & {
+  time?: boolean
+  format?: string
+}
+
+
+export const DatePicker = ({ time, format, ...props }: DatePickerProps) => {
+  const formatString = format || (time ? 'YYYY-MM-DD, HH:mm:ss' : 'YYYY-MM-DD')
+
   return (
     <TextInput
-      onRender={(value) => dayjs(value).format('YYYY-MM-DD')}
+      onRender={(value) => dayjs(value).format(formatString)}
       onDisplayValue={(value) => {
         let date = dayjs(value)
         if (!date.isValid()) {
           return value
         }
-        return date.format('DD MMM YYYY')
+        return date.format(formatString)
       }}
       onChange={((value) => dayjs(value).toISOString())}
       {...props}
